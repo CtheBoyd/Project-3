8420f0ef62466de256b5c878f2ebe7525ba3cd22
CtheBoyd <99106917+CtheBoyd@users.noreply.github.com>
2022-04-13 15:28:16 -0700

    update

--- num_guess.py
+++ num_guess.py
@@ -3,20 +3,21 @@
 # Description: Two players. First player chooses an integer. Second player has to guess it.
 #
 
-print ("Enter the integer for the player to guess.")
-player_1 = int(input())
+print("Enter the integer for the player to guess.")
+num1 = int(input())
 print("Enter your guess.")
-player_2 = int(input())
-attempts = 0
-while player_1 != player_2:
-    attempts += 1
-    if player_2 > player_1:
-        print ("Too high - try again:")
-        player_2 = int(input())
-    elif player_2 < player_1:
-        print ("Too low - try again:")
-        player_2 = int(input())
-if player_2 == player_1:
-    attempts += 1
-    print ("you guessed it in", attempts, "tries.")
+num2 = int(input())
+num3 = 1
+while num1 != num2:
+    num3 += 1
+    if num2 > num1:
+        print("Too high - try again:")
+        num2 = int(input())
+    elif num2 < num1:
+         print("Too low - try again:")
+         num2 = int(input())
+if num3 == 1:
+    print("you guessed it in 1 try.")
+else:
+    print("you guessed it in", num3 ,"tries.")
 
