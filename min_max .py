6d0840ddaa4818d1bc5f11677a28ce5a04634efd
CtheBoyd <99106917+CtheBoyd@users.noreply.github.com>
2022-04-12 20:51:58 -0700

    test

--- min_max .py
+++ min_max .py
@@ -1,8 +1,13 @@
 # Author: Christopher Boyd
 # GitHub username: CtheBoyd
 # Date: 4/4/2022
-# Description: Asks user to provide five numbers and then provides an average.
+# Description: Returns max/min on user chosen, at random, number of user inputs.
 #
 print ("How many integers would you like to enter?")
-x = float(input())
-print ("Please enter", int(x), "integers.")
+num = int(input())
+print ("Please enter", num, "integers.")
+
+
+
+
+
