b83b7611c1d807c541d28a34634215d3365f162d
CtheBoyd <99106917+CtheBoyd@users.noreply.github.com>
2022-04-13 14:22:43 -0700

    update

--- min_max.py
+++ min_max.py
@@ -3,8 +3,9 @@
 # Date: 4/12/2022
 # Description: Returns max/min on user chosen, at random, number of user inputs.
 #
-input_1 = int(input(("How many integerts would you like to enter?\n")))
-print("Please enter" + " " + str(input_1) + " " + "integers.")
+print ("How many integers would you like to enter?")
+input_1 = int(input())
+print("Please enter", input_1, " integers.")
 min = 1.7976931348623157e+308
 max = -1.7976931348623157e+308
 for i in range(0, input_1):
