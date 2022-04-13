0c62a54e1623a768623ca2b8263a852e1f4fe715
CtheBoyd <99106917+CtheBoyd@users.noreply.github.com>
2022-04-13 13:46:01 -0700

    update

--- factors.py
+++ factors.py
@@ -2,7 +2,6 @@
 # Date: 4/12/2022
 # Description: Returns the factors of a user provided positive integer.
 #
-
 input_1 = int(input("Please enter a positive integer: "))
 print ("The factors of", input_1, "are:")
 for num in range(1, input_1 +1):
